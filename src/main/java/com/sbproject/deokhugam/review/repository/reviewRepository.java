287df7a1ecff52379806516a02cb2b6edb60405c
kyhun1007 <kyhun1007@naver.com>
2026-06-25 17:52:57 +0900

    feat: review 도메인 기본 클래스 생성

---
+++ src/main/java/com/sbproject/deokhugam/review/repository/reviewRepository.java
@@ -0,0 +1,4 @@
+package com.sbproject.deokhugam.review.repository;
+
+public interface reviewRepository {
+}
---
+++ src/main/java/com/sbproject/deokhugam/review/service/Impl/reviewServiceImpl.java
@@ -0,0 +1,9 @@
+package com.sbproject.deokhugam.review.service.Impl;
+
+import org.springframework.stereotype.Service;
+
+import com.sbproject.deokhugam.review.service.reviewService;
+
+@Service
+public class reviewServiceImpl implements reviewService {
+}
