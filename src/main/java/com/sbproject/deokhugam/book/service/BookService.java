fe860deb1c98e297f2076e07476530ad5dca4889
seongjuun <beajon0301@naver.com>
2026-06-26 19:27:37 +0900

    feat: 도서 등록 기능 구현

--- src/main/java/com/sbproject/deokhugam/book/service/BookService.java
+++ src/main/java/com/sbproject/deokhugam/book/service/BookService.java
@@ -3,14 +3,23 @@ package com.sbproject.deokhugam.book.service;
 import java.time.Instant;
 import java.util.UUID;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import com.sbproject.deokhugam.book.dto.BookCreateRequest;
 import com.sbproject.deokhugam.book.dto.BookDto;
 import com.sbproject.deokhugam.book.dto.BookOrderBy;
 import com.sbproject.deokhugam.book.dto.CursorPageResponseBookDto;
 import com.sbproject.deokhugam.book.dto.Direction;
+import com.sbproject.deokhugam.common.dto.SlicePageResponse;
+
+import jakarta.validation.Valid;
 
 public interface BookService {
-	CursorPageResponseBookDto searchBooks(String keyword, BookOrderBy orderBy, Direction direction, String cursor,
-	                                    Instant after, int limit);
+	SlicePageResponse<BookDto> searchBooks(String keyword, BookOrderBy orderBy, Direction direction, String cursor,
+	                                       Instant after, int limit);
+
+	BookDto createBook(@Valid BookCreateRequest request, MultipartFile thumbnailImage);
 
 	BookDto getBook(UUID bookId);
+
 }
--- src/main/java/com/sbproject/deokhugam/book/service/impl/BookServiceImpl.java
+++ src/main/java/com/sbproject/deokhugam/book/service/impl/BookServiceImpl.java
@@ -4,10 +4,13 @@ import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.sbproject.deokhugam.book.dto.BookCreateRequest;
 import com.sbproject.deokhugam.book.dto.BookDto;
 import com.sbproject.deokhugam.book.dto.BookOrderBy;
 import com.sbproject.deokhugam.book.dto.CursorPageResponseBookDto;
@@ -17,6 +20,7 @@ import com.sbproject.deokhugam.book.exception.BookNotFoundException;
 import com.sbproject.deokhugam.book.mapper.BookMapper;
 import com.sbproject.deokhugam.book.repository.BookRepository;
 import com.sbproject.deokhugam.book.service.BookService;
+import com.sbproject.deokhugam.common.dto.SlicePageResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -29,8 +33,9 @@ public class BookServiceImpl implements BookService {
 	private final BookMapper bookMapper;
 
 	@Override
-	public CursorPageResponseBookDto searchBooks(String keyword, BookOrderBy orderBy, Direction direction, String cursor,
-	                                           Instant after, int limit) {
+	public SlicePageResponse<BookDto> searchBooks(String keyword, BookOrderBy orderBy, Direction direction,
+	                                              String cursor,
+	                                              Instant after, int limit) {
 		Long totalElements = (keyword == null || keyword.isBlank())
 			? bookRepository.count()
 			: bookRepository.countByKeyword(keyword);
@@ -38,10 +43,10 @@ public class BookServiceImpl implements BookService {
 		Slice<Book> books = bookRepository.searchBooks(keyword, orderBy, direction, cursor, after, limit);
 		int size = books.getContent().size();
 		List<BookDto> bookDtoList = books.stream()
-		                                .map(bookMapper::toBookDto)
-		                                .toList();
+		                                 .map(bookMapper::toBookDto)
+		                                 .toList();
 		if (bookDtoList.isEmpty()) {
-			return new CursorPageResponseBookDto(List.of(), null, null, 0, totalElements, false);
+			return new SlicePageResponse<BookDto>(List.of(), null, null, 0, totalElements, false);
 		}
 		BookDto lastBook = bookDtoList.get(size - 1);
 		String nextCursor = switch (orderBy) {
@@ -51,8 +56,28 @@ public class BookServiceImpl implements BookService {
 			case reviewCount -> String.valueOf(lastBook.reviewCount());
 		};
 
-		return new CursorPageResponseBookDto(bookDtoList, nextCursor, lastBook.createdAt(),
-											 size, totalElements, books.hasNext());
+		return new SlicePageResponse<BookDto>(bookDtoList, nextCursor, lastBook.createdAt(),
+		                                     size, totalElements, books.hasNext());
+	}
+
+	@Override
+	@Transactional
+	public BookDto createBook(@NonNull BookCreateRequest request, MultipartFile thumbnailImage) {
+		Book book = Book.builder()
+		                .isbn(request.isbn())
+		                .title(request.title())
+		                .author(request.author())
+		                .description(request.description())
+		                .publisher(request.publisher())
+		                .publishedDate(request.publishedDate())
+		                .thumbnailUrl(
+			                thumbnailImage != null && !thumbnailImage.isEmpty() ? thumbnailImage.getName() : null)
+		                .reviewCount(0)
+		                .totalScore(0)
+		                .rating(0.0)
+		                .build();
+		Book savedBook = bookRepository.save(book);
+		return bookMapper.toBookDto(savedBook);
 	}
 
 	@Override
