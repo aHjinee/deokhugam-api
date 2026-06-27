05f69626e7ef46da602bceed1aae798ed355540f
Sohyun Ahn <snini7355@naver.com>
2026-06-27 16:06:33 +0900

    refactor: CommentNotOwnedException 클래스명 오타 수정

--- src/main/java/com/sbproject/deokhugam/comments/exception/CommentNotOwnedException.java
+++ src/main/java/com/sbproject/deokhugam/comments/exception/CommentNotOwnedException.java
@@ -3,7 +3,7 @@ package com.sbproject.deokhugam.comments.exception;
 import com.sbproject.deokhugam.common.exception.BaseException;
 import com.sbproject.deokhugam.common.exception.ErrorCode;
 
-public class gCommentNotOwnedException extends BaseException {
+public class CommentNotOwnedException extends BaseException {
 
 	public CommentNotOwnedException() {
 		super(ErrorCode.COMMENT_NOT_OWNED);
